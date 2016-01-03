b42d062c8bf9387b3614cd56e029ee0c347f9f29
meriamsajida <meriam92mairem@gmail.com>
2016-01-03 01:04:58 +0100

    Mise à jour de Readme avec la structure du projet

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var appExpress = express();
 var animalController = require('./server/controllers/animalController');
 animalController(appExpress);
 
-//appExpress.use(express.static(__dirname + '/public'));
 appExpress.use("/", express.static(__dirname + '/public'));
 
 // Ecouter les requetes HTTP sur le port 8888
